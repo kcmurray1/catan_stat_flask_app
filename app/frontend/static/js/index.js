cffa40f17706a5a05e86f1fa31503c04eff194df
kcmurray1 <kristiancmurray@gmail.com>
2025-03-29 16:30:10 -0700

    Created Bootstrap class and implement basic logic for game element

--- app/frontend/static/js/index.js
+++ app/frontend/static/js/index.js
@@ -3,18 +3,8 @@ const DEFAULT_COLORS = ["Blue", "Green", "Red", "Orange", "Brown", "White"]
 // Display 10 initial books
 document.addEventListener("DOMContentLoaded", function()
 {
-    
     var game = new Game()
-        
-
 });
-
-function move()
-{
-    console.log("hello world");
-    document.getElementById("game").innerHTML = "nice job!";
-}
-
 
 function CreateElement({name, id, classList, innerHTML=null})
 {
@@ -38,6 +28,78 @@ function CreateElement({name, id, classList, innerHTML=null})
     return newElement;
 }
 
+class BootStrapCard
+{
+    constructor ({id, header, body, title, text, footer})
+    {
+        this.cardContainer = CreateElement({name: "div", id: id, classList: ["card", "bg-light", "mb-3"]});
+
+        this.cardHeader = CreateElement({
+            name: "h1",
+            classList: "card-header",
+            innerHTML: header
+        });
+
+        this.cardBody = CreateElement({
+            name: "div",
+            classList: "card-body",
+            innerHTML: body
+        });
+
+        this.cardTitle = CreateElement({
+            name: "h5",
+            classList: "card-title",
+            innerHTML: title
+        });
+
+
+        this.cardText = CreateElement({
+            name: "p",
+            classList: "card-text",
+            innerHTML: text
+        });
+
+        this.cardFooter = CreateElement({
+            name: "div",
+            classList: "card-footer",
+            innerHTML: footer
+        });
+
+        
+        this.cardBody.appendChild(this.cardTitle);
+        this.cardBody.appendChild(this.cardText);
+
+
+        this.cardContainer.appendChild(this.cardHeader);
+
+        this.cardContainer.appendChild(this.cardBody);
+        this.cardContainer.appendChild(this.cardFooter);
+
+    }
+
+    updateHeader(content)
+    {
+        this.cardHeader.innerHTML = content;
+    }
+
+    updateBody(content)
+    {
+        this.cardBody.innerHTML = content;
+    }
+
+    updateTitle(content)
+    {
+        this.cardTitle.innerHTML = content;
+    }
+
+    updateText(content)
+    {
+        this.cardText.innerHTML = content;
+    }
+}
+
+
+
 class Game
 {
    
@@ -45,16 +107,18 @@ class Game
     {
         // Create Initial UI
         this.players = [];
+        this.currentPlayer = 0;
 
         this.element = this.buildUI();
+        this.hasGameStarted = false;
 
     }
 
     buildUI()
     {
 
-        let element = CreateElement(
-            {name: "section",
+        let element = CreateElement({
+            name: "section",
             id: "game",
             classList: "game-container"
         });
@@ -64,35 +128,60 @@ class Game
             classList : "row"
         });
         
-        this.unselectedPlayers = CreateElement({
+        this.leftContainer = CreateElement({
             name: "div",
             id: "gub",
-            classList : "col-sm-8",
-            innerHTML : `<h1>Select Players</h1>`
+            classList : "col-sm-6"
         })
 
+        this.leftCard = new BootStrapCard({
+            header: "Available Colors",  
+        })
 
-          for(let color of DEFAULT_COLORS)
-            {
-                let player = new PlayerButton(color);
-                player.onAdd = () => {this.addPlayer(player)}
-                player.onRemove = () =>{this.removePlayer(player)}
+        this.leftContainer.appendChild(this.leftCard.cardContainer);
 
-                this.unselectedPlayers.appendChild(player.element);
-            }
+
+
+
+        for(let color of DEFAULT_COLORS)
+        {
+            let player = new PlayerButton(color);
+            player.onAdd = () => {this.addPlayer(player)}
+            player.onRemove = () =>{this.removePlayer(player)}
+
+            this.leftCard.cardTitle.appendChild(player.element);
+        }
         
         
-        this.rightElement = CreateElement({
+        this.rightContainer = CreateElement({
             name: "div",
             id: "boo",
-            classList : "col-sm-4",
-            innerHTML : `<h1>Players</h1>`
+            classList : "col-sm-6"
         })
 
+        this.rightCard = new BootStrapCard({
+            header: "Selected Player",
+            
+        })
+        let nextButton =  CreateElement({
+            name: "button",
+            id: "button",
+            classList: ["btn" ,"btn-dark"],
+            innerHTML: "Next"
+        })
+        nextButton.onmousedown = () => {this.nextMove()};
+        this.rightCard.cardFooter.appendChild(
+            nextButton
+        );
+       
 
-        elementRow.appendChild(this.unselectedPlayers);
+        this.rightContainer.appendChild(this.rightCard.cardContainer);
         
-        elementRow.appendChild(this.rightElement);
+
+
+        elementRow.appendChild(this.leftContainer);
+        
+        elementRow.appendChild(this.rightContainer);
         element.appendChild(elementRow);
 
         let idk = document.getElementById("man");
@@ -108,7 +197,8 @@ class Game
     addPlayer(player)
     {
         this.players.push(player);
-        this.rightElement.appendChild(player.element);
+        // player.element.innerHTML = `<p>${this.players.findIndex(function(item){return item.element.id == player.element.id}) + 1}<p>`;
+        this.rightCard.cardTitle.appendChild(player.element);
     }
 
     removePlayer(player)
@@ -117,7 +207,57 @@ class Game
             return item.element.id != player.element.id;
         })
 
-        this.unselectedPlayers.appendChild(player.element);
+        this.leftCard.cardTitle.appendChild(player.element);
+    }
+
+    nextMove()
+    {
+        if(this.players.length < 1){return;}
+        if(!this.hasGameStarted)
+        {
+            for(let player of this.players)
+            {
+                player.removeListeners();
+            }
+
+            this.gameUI();
+        }
+        // Mark game as started
+        this.hasGameStarted = true;
+
+        // Update display for current Player
+        let activePlayer = this.players[this.currentPlayer];
+        // left card displays the current player and is where user can enter information
+        this.gameUpdateLeftCard(activePlayer);
+        // right card displays current stats
+        this.gameUpdateRightCard(activePlayer);
+        
+
+        // Update player index
+        this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+    }
+
+
+    gameUI()
+    {
+        // Update headers
+        this.leftCard.updateHeader("Active Player");
+        this.rightCard.updateHeader("Player Stats");
+    }
+
+    gameUpdateRightCard(player)
+    {
+        this.rightCard.cardTitle.replaceChildren(player.element.id);
+
+    }
+
+    gameUpdateLeftCard(player)
+    {
+        this.leftCard.updateTitle(
+            `hello<br>`
+        );
+        this.leftCard.cardTitle.appendChild(player.element);
+
     }
 
 
@@ -141,7 +281,6 @@ class PlayerButton
         this.onAdd = () => {}
         this.onRemove = () => {}
 
-        this.handle
         this.element.onmousedown = () =>{
             if(!this.isPartOfGame)
             {
@@ -157,5 +296,14 @@ class PlayerButton
         this.element.onmouseenter = () =>{this.element.style.opacity = 0.75}
         
         this.element.onmouseleave = () =>{this.element.style.opacity = 1}
+    }
+
+    removeListeners()
+    {
+        this.element.onmousedown = () =>{};
+        this.element.onmouseenter = () =>{};
+        
+        this.element.onmouseleave = () =>{};
+        this.element.disabled = true;
     }
 }
